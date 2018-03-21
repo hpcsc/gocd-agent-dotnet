0c24249074a26173a418e8d94882363f0435ee04
David Nguyen <hpcsc@users.noreply.github.com>
2018-03-21 23:35:41 +0800

    use single dockerfile with arg

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ These docker images install .NET Core SDK on top of GoCD Agent images
     - GoCD Agent v18.2.0
     - .NET Core SDK v2.1
     - Docker CE v17.12.0
+
+*Note:* this image installs Docker inside a Docker container. It needs to be started with docker socket from host machine mounted as a volume, .e.g:
+
+```
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock hpcsc/gocd-agent-dotnet:gocd-18.2.0-ubuntu-16.04-net-2.1
+```
+
+For more details: [https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/](https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/)
--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-DOCKERFILE=$1
-TAG=$2
+GOCD_VERSION=$1
+NETCORE_PACKAGE=$2
+DOCKER_CE_PACKAGE=$3
+TAG=$4
 
-if [[ ( $DOCKERFILE = "" ) || ( $TAG = "" ) ]]; then
-    echo "Usage: ./build.sh [path_to_docker_file] [tag]"
-    echo "Example: ./build.sh ./2.1/gocd-18.2.0-ubuntu-16.04/Dockerfile gocd-18.2.0-ubuntu-16.04-net-2.1"
+if [[ ( $GOCD_VERSION = "" ) || ( $NETCORE_PACKAGE = "" ) || ( $DOCKER_CE_PACKAGE = "" ) || ( $TAG = "" ) ]]; then
+    echo "Usage: ./build.sh [GoCD_version] [.NET_Core_apt_package_version] [Docker_CE_apt_package_version] [Tag]"
+    echo "Example: ./build.sh v18.2.0 dotnet-sdk-2.1.101 17.12.0~ce-0~ubuntu gocd-18.2.0-ubuntu-16.04-net-2.1"
     exit 1
 fi;
 
-docker build -f $DOCKERFILE -t hpcsc/gocd-agent-dotnet:$TAG .
+docker build --build-arg GOCD_VERSION=$GOCD_VERSION \
+             --build-arg NETCORE_PACKAGE=$NETCORE_PACKAGE \
+             --build-arg DOCKER_CE_PACKAGE=$DOCKER_CE_PACKAGE \
+             -t hpcsc/gocd-agent-dotnet:$TAG \
+             .
